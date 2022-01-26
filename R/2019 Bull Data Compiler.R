a0d0a2711e9667d8d94cf94bb0e10b67e233c3fa
Drew Di Francesco <drewdifrancesco@gmail.com>
2022-01-26 15:30:36 -0500

    minor update

--- R/2019 Bull Data Compiler.R
+++ R/2019 Bull Data Compiler.R
@@ -5,8 +5,7 @@ library(dplyr)
 
 
 #Bull 1
-Bull1JanMar19=read.csv("rawData/Bull1/Bull1JanMar19.txt", header=FALSE)
-Bull1JanMar19$V15=gsub("'","")
+
 Bull1Apr19 = read.csv("rawData/Bull1/Bull1Apr19.csv")
 Bull1May19 = read.csv("rawData/Bull1/Bull1May19.csv")
 Bull1Jun19 = read.csv("rawData/Bull1/Bull1Jun19.csv")
@@ -29,13 +28,13 @@ Bull1a$doy <- as.numeric(strftime(Bull1a$ï..Time, format = "%j"))   #creating v
 
 
 #Bull 2
-Bull2May19 = read.csv("Bull2/Bull2.1May19.csv")
-Bull2Jun19 = read.csv("Bull2/Bull2.1Jun19.csv")
-Bull2Jul19 = read.csv("Bull2/Bull2.1Jul19.csv")
-Bull2Aug19 = read.csv("Bull2/Bull2.1Aug19.csv")
-Bull2Oct19 = read.csv("Bull2/Bull2.1Oct19.csv")
-Bull2Nov19 = read.csv("Bull2/Bull2.1Nov19.csv")
-Bull2Dec19 = read.csv("Bull2/Bull2.1Dec19.csv")
+Bull2May19 = read.csv("rawData/Bull2/Bull2.1May19.csv")
+Bull2Jun19 = read.csv("rawData/Bull2/Bull2.1Jun19.csv")
+Bull2Jul19 = read.csv("rawData/Bull2/Bull2.1Jul19.csv")
+Bull2Aug19 = read.csv("rawData/Bull2/Bull2.1Aug19.csv")
+Bull2Oct19 = read.csv("rawData/Bull2/Bull2.1Oct19.csv")
+Bull2Nov19 = read.csv("rawData/Bull2/Bull2.1Nov19.csv")
+Bull2Dec19 = read.csv("rawData/Bull2/Bull2.1Dec19.csv")
 
 Bull2=rbind(Bull2May19[c(1,4,5)],Bull2Jun19[c(1,4,5)],Bull2Jul19[c(1,4,5)],Bull2Aug19[c(1,4,5)],Bull2Oct19[c(1,4,5)],Bull2Nov19[c(1,4,5)],Bull2Dec19[c(1,4,5)])
 
@@ -48,14 +47,14 @@ Bull2a$doy <- as.numeric(strftime(Bull2a$ï..Time, format = "%j"))
 
 
 #Bull 3
-Bull3Apr19 = read.csv("Bull3/Bull3Apr19.csv")
-Bull3May19 = read.csv("Bull3/Bull3May19.csv")
-Bull3Jun19 = read.csv("Bull3/Bull3Jun19.csv")
-Bull3Jul19 = read.csv("Bull3/Bull3Jul19.csv")
-Bull3Aug19 = read.csv("Bull3/Bull3Aug19.csv")
-Bull3Oct19 = read.csv("Bull3/Bull3Oct19.csv")
-Bull3Nov19 = read.csv("Bull3/Bull3Nov19.csv")
-Bull3Dec19 = read.csv("Bull3/Bull3Dec19.csv")
+Bull3Apr19 = read.csv("rawData/Bull3/Bull3Apr19.csv")
+Bull3May19 = read.csv("rawData/Bull3/Bull3May19.csv")
+Bull3Jun19 = read.csv("rawData/Bull3/Bull3Jun19.csv")
+Bull3Jul19 = read.csv("rawData/Bull3/Bull3Jul19.csv")
+Bull3Aug19 = read.csv("rawData/Bull3/Bull3Aug19.csv")
+Bull3Oct19 = read.csv("rawData/Bull3/Bull3Oct19.csv")
+Bull3Nov19 = read.csv("rawData/Bull3/Bull3Nov19.csv")
+Bull3Dec19 = read.csv("rawData/Bull3/Bull3Dec19.csv")
 
 Bull3=rbind(Bull3May19[c(1,4,5)],Bull3Jun19[c(1,4,5)],Bull3Jul19[c(1,4,5)],Bull3Aug19[c(1,4,5)],Bull3Oct19[c(1,4,5)],Bull3Nov19[c(1,4,5)],Bull3Dec19[c(1,4,5)])
 
@@ -68,13 +67,13 @@ Bull3a$doy <- as.numeric(strftime(Bull3a$ï..Time, format = "%j"))
 
 
 #Bull 4
-Bull4May19 = read.csv("Bull4/Bull4May19.csv")
-Bull4Jun19 = read.csv("Bull4/Bull4Jun19.csv")
-Bull4Jul19 = read.csv("Bull4/Bull4Jul19.csv")
-Bull4Aug19 = read.csv("Bull4/Bull4Aug19.csv")
-Bull4Oct19 = read.csv("Bull4/Bull4Oct19.csv")
-Bull4Nov19 = read.csv("Bull4/Bull4Nov19.csv")
-Bull4Dec19 = read.csv("Bull4/Bull4Dec19.csv")
+Bull4May19 = read.csv("rawData/Bull4/Bull4May19.csv")
+Bull4Jun19 = read.csv("rawData/Bull4/Bull4Jun19.csv")
+Bull4Jul19 = read.csv("rawData/Bull4/Bull4Jul19.csv")
+Bull4Aug19 = read.csv("rawData/Bull4/Bull4Aug19.csv")
+Bull4Oct19 = read.csv("rawData/Bull4/Bull4Oct19.csv")
+Bull4Nov19 = read.csv("rawData/Bull4/Bull4Nov19.csv")
+Bull4Dec19 = read.csv("rawData/Bull4/Bull4Dec19.csv")
 
 Bull4=rbind(Bull4May19[c(1,4,5)],Bull4Jun19[c(1,4,5)],Bull4Jul19[c(1,4,5)],Bull4Aug19[c(1,4,5)],Bull4Oct19[c(1,4,5)],Bull4Nov19[c(1,4,5)],Bull4Dec19[c(1,4,5)])
 
@@ -87,13 +86,13 @@ Bull4a$doy <- as.numeric(strftime(Bull4a$ï..Time, format = "%j"))
 
 
 #Bull 5
-Bull5May19 = read.csv("Bull5/Bull5May19.csv")
-Bull5Jun19 = read.csv("Bull5/Bull5Jun19.csv")
-Bull5Jul19 = read.csv("Bull5/Bull5Jul19.csv")
-Bull5Aug19 = read.csv("Bull5/Bull5Aug19.csv")
-Bull5Oct19 = read.csv("Bull5/Bull5Oct19.csv")
-Bull5Nov19 = read.csv("Bull5/Bull5Nov19.csv")
-Bull5Dec19 = read.csv("Bull5/Bull5Dec19.csv")
+Bull5May19 = read.csv("rawData/Bull5/Bull5May19.csv")
+Bull5Jun19 = read.csv("rawData/Bull5/Bull5Jun19.csv")
+Bull5Jul19 = read.csv("rawData/Bull5/Bull5Jul19.csv")
+Bull5Aug19 = read.csv("rawData/Bull5/Bull5Aug19.csv")
+Bull5Oct19 = read.csv("rawData/Bull5/Bull5Oct19.csv")
+Bull5Nov19 = read.csv("rawData/Bull5/Bull5Nov19.csv")
+Bull5Dec19 = read.csv("rawData/Bull5/Bull5Dec19.csv")
 
 Bull5=rbind(Bull5May19[c(1,4,5)],Bull5Jun19[c(1,4,5)],Bull5Jul19[c(1,4,5)],Bull5Aug19[c(1,4,5)],Bull5Oct19[c(1,4,5)],Bull5Nov19[c(1,4,5)],Bull5Dec19[c(1,4,5)])
 
@@ -106,13 +105,13 @@ Bull5a$doy <- as.numeric(strftime(Bull5a$ï..Time, format = "%j"))
 
 
 #Bull 6 
-Bull6May19 = read.csv("Bull6/Bull6May19.csv")
-Bull6Jun19 = read.csv("Bull6/Bull6Jun19.csv")
-Bull6Jul19 = read.csv("Bull6/Bull6Jul19.csv")
-Bull6Aug19 = read.csv("Bull6/Bull6Aug19.csv")
-Bull6Oct19 = read.csv("Bull6/Bull6Oct19.csv")
-Bull6Nov19 = read.csv("Bull6/Bull6Nov19.csv")
-Bull6Dec19 = read.csv("Bull6/Bull6Dec19.csv")
+Bull6May19 = read.csv("rawData/Bull6/Bull6May19.csv")
+Bull6Jun19 = read.csv("rawData/Bull6/Bull6Jun19.csv")
+Bull6Jul19 = read.csv("rawData/Bull6/Bull6Jul19.csv")
+Bull6Aug19 = read.csv("rawData/Bull6/Bull6Aug19.csv")
+Bull6Oct19 = read.csv("rawData/Bull6/Bull6Oct19.csv")
+Bull6Nov19 = read.csv("rawData/Bull6/Bull6Nov19.csv")
+Bull6Dec19 = read.csv("rawData/Bull6/Bull6Dec19.csv")
 
 Bull6=rbind(Bull6May19[c(1,4,5)],Bull6Jun19[c(1,4,5)],Bull6Jul19[c(1,4,5)],Bull6Aug19[c(1,4,5)],Bull6Oct19[c(1,4,5)],Bull6Nov19[c(1,4,5)],Bull6Dec19[c(1,4,5)])
 
@@ -124,6 +123,116 @@ Bull6a=Bull6[n,]
 Bull6a$doy <- as.numeric(strftime(Bull6a$ï..Time, format = "%j"))
 
 
+#Bull 7 
+Bull7Oct19 = read.csv("rawData/Bull7/Bull7Oct19.csv")
+Bull7Nov19 = read.csv("rawData/Bull7/Bull7Nov19.csv")
+Bull7Dec19 = read.csv("rawData/Bull7/Bull7Dec19.csv")
+
+Bull7=rbind(Bull7Oct19[c(1,4,5)],Bull7Nov19[c(1,4,5)],Bull7Dec19[c(1,4,5)])
+
+Bull7=Bull7[order(Bull7$ï..Time),] 
+
+n=1:(nrow(Bull7)/4)*4
+Bull7a=Bull7[n,]  
+
+Bull7a$doy <- as.numeric(strftime(Bull7a$ï..Time, format = "%j"))
+
+
+#Bull 8 
+
+Bull8Oct19 = read.csv("rawData/Bull8/Bull8Oct19.csv")
+Bull8Nov19 = read.csv("rawData/Bull8/Bull8Nov19.csv")
+Bull8Dec19 = read.csv("rawData/Bull8/Bull8Dec19.csv")
+
+Bull8=rbind(Bull8Oct19[c(1,4,5)],Bull8Nov19[c(1,4,5)],Bull8Dec19[c(1,4,5)])
+
+Bull8=Bull8[order(Bull8$ï..Time),] 
+
+n=1:(nrow(Bull8)/4)*4
+Bull8a=Bull8[n,]  
+
+Bull8a$doy <- as.numeric(strftime(Bull8a$ï..Time, format = "%j"))
+
+
+#Bull 9 
+
+Bull9Oct19 = read.csv("rawData/Bull9/Bull9Oct19.csv")
+Bull9Nov19 = read.csv("rawData/Bull9/Bull9Nov19.csv")
+Bull9Dec19 = read.csv("rawData/Bull9/Bull9Dec19.csv")
+
+Bull9=rbind(Bull9Oct19[c(1,4,5)],Bull9Nov19[c(1,4,5)],Bull9Dec19[c(1,4,5)])
+
+Bull9=Bull9[order(Bull9$ï..Time),] 
+
+n=1:(nrow(Bull9)/4)*4
+Bull9a=Bull9[n,]  
+
+Bull9a$doy <- as.numeric(strftime(Bull9a$ï..Time, format = "%j"))
+
+
+#Bull 10 
+
+Bull10Oct19 = read.csv("rawData/Bull10/Bull10Oct19.csv")
+Bull10Nov19 = read.csv("rawData/Bull10/Bull10Nov19.csv")
+Bull10Dec19 = read.csv("rawData/Bull10/Bull10Dec19.csv")
+
+Bull10=rbind(Bull10Oct19[c(1,4,5)],Bull10Nov19[c(1,4,5)],Bull10Dec19[c(1,4,5)])
+
+Bull10=Bull10[order(Bull10$ï..Time),] 
+
+n=1:(nrow(Bull10)/4)*4
+Bull10a=Bull10[n,]  
+
+Bull10a$doy <- as.numeric(strftime(Bull10a$ï..Time, format = "%j"))
+
+
+#Bull 12 
+
+Bull12Oct19 = read.csv("rawData/Bull12/Bull12Oct19.csv")
+Bull12Nov19 = read.csv("rawData/Bull12/Bull12Nov19.csv")
+Bull12Dec19 = read.csv("rawData/Bull12/Bull12Dec19.csv")
+
+Bull12=rbind(Bull12Oct19[c(1,4,5)],Bull12Nov19[c(1,4,5)],Bull12Dec19[c(1,4,5)])
+
+Bull12=Bull12[order(Bull12$ï..Time),] 
+
+n=1:(nrow(Bull12)/4)*4
+Bull12a=Bull12[n,]  
+
+Bull12a$doy <- as.numeric(strftime(Bull12a$ï..Time, format = "%j"))
+
+
+#Bull 13
+
+Bull13Oct19 = read.csv("rawData/Bull13/Bull13Oct19.csv")
+Bull13Nov19 = read.csv("rawData/Bull13/Bull13Nov19.csv")
+Bull13Dec19 = read.csv("rawData/Bull13/Bull13Dec19.csv")
+
+Bull13=rbind(Bull13Oct19[c(1,4,5)],Bull13Nov19[c(1,4,5)],Bull13Dec19[c(1,4,5)])
+
+Bull13=Bull13[order(Bull13$ï..Time),] 
+
+n=1:(nrow(Bull13)/4)*4
+Bull13a=Bull13[n,]  
+
+Bull13a$doy <- as.numeric(strftime(Bull13a$ï..Time, format = "%j"))
+
+
+#Bull 14
+
+Bull14Nov19 = read.csv("rawData/Bull14/Bull14Nov19.csv")
+Bull14Dec19 = read.csv("rawData/Bull14/Bull14Dec19.csv")
+
+Bull14=rbind(Bull14Nov19[c(1,4,5)],Bull14Dec19[c(1,4,5)])
+
+Bull14=Bull14[order(Bull14$ï..Time),] 
+
+n=1:(nrow(Bull14)/4)*4
+Bull14a=Bull14[n,]  
+
+Bull14a$doy <- as.numeric(strftime(Bull14a$ï..Time, format = "%j"))
+
+
 #Combining Dataset
 Bull1 <- Bull1a %>% add_column(BulNum = "1")
 Bull2 <- Bull2a %>% add_column(BulNum = "2")
@@ -131,10 +240,17 @@ Bull3 <- Bull3a %>% add_column(BulNum = "3")
 Bull4 <- Bull4a %>% add_column(BulNum = "4")
 Bull5 <- Bull5a %>% add_column(BulNum = "5")
 Bull6 <- Bull6a %>% add_column(BulNum = "6")
+Bull7 <- Bull7a %>% add_column(BulNum = "7")
+Bull8 <- Bull8a %>% add_column(BulNum = "8")
+Bull9 <- Bull9a %>% add_column(BulNum = "9")
+Bull10 <- Bull10a %>% add_column(BulNum = "10")
+Bull12 <- Bull12a %>% add_column(BulNum = "12")
+Bull13 <- Bull13a %>% add_column(BulNum = "13")
+Bull14 <- Bull14a %>% add_column(BulNum = "14")
 
 
-Bull <- rbind(Bull1[c(1,2,3,4,5)],Bull2[c(1,2,3,4,5)],Bull3[c(1,2,3,4,5)],Bull4[c(1,2,3,4,5)],Bull5[c(1,2,3,4,5)],Bull6[c(1,2,3,4,5)])
+Bull <- rbind(Bull1[c(1,2,3,4,5)],Bull2[c(1,2,3,4,5)],Bull3[c(1,2,3,4,5)],Bull4[c(1,2,3,4,5)],Bull5[c(1,2,3,4,5)],Bull6[c(1,2,3,4,5)],Bull7[c(1,2,3,4,5)],Bull8[c(1,2,3,4,5)],Bull9[c(1,2,3,4,5)],Bull10[c(1,2,3,4,5)],Bull12[c(1,2,3,4,5)],Bull13[c(1,2,3,4,5)],Bull14[c(1,2,3,4,5)])
 Bull=Bull[order(Bull$ï..Time),] 
 
-write.csv(Bull,"BullData2019.csv")
+#write.csv(Bull,"BullData2019.csv")
 
