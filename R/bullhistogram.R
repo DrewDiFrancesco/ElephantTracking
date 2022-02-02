0af3e894ae29bdb3e1cbd86c217be225919a4f3b
Drew Di Francesco <drewdifrancesco@gmail.com>
2022-02-01 19:38:31 -0500

    colored histogram by year

--- R/bullhistogram.R
+++ R/bullhistogram.R
@@ -6,5 +6,7 @@ names(bull2020)[2] <- 'Time'
 bull2021 <- read.csv("data/BullData2021.csv")
 
 bigbull <- rbind(bull2019[c(2,3,4,5,6)],bull2020[c(2,3,4,5,6)],bull2021[c(2,3,4,5,6)])
+bigbull$year <- substr(bigbull$Time, 0, 4)
 
-ggplot(bigbull, aes(x = BulNum)) + geom_histogram(colour = 4, fill = 'grey', bins = 15, binwidth = .5) + xlab("Bull Number") + ggtitle("Number of Tracking Data Points per Bull") + theme_classic()
+ggplot(bigbull, aes(x = BulNum, fill = year)) + geom_histogram(bins = 15, binwidth = .5) + xlab("Bull Number") + 
+  ylab("") + ggtitle("Number of Tracking Data Points per Bull") 
