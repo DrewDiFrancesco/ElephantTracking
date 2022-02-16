232e3bc5bb23f6ae8a6f5dc25095db649dd6aeef
benkoschella <benkoschella@gmail.com>
2022-02-15 20:16:04 -0500

    Create Testing Euclidian Distance.R

---
+++ R/Testing Euclidian Distance.R
@@ -0,0 +1,20 @@
+BullData2021 <- read.csv("~/GitHub/ElephantTracking/data/BullData2021.csv")
+View(BullData2021)
+
+Distance=function(bull,other){
+  BullLat=subset(BullData2021,BulNum==bull)$Latitude
+  BullLong=subset(BullData2021,BulNum==bull)$Longitude
+  OthLat=other$Latitude
+  OthLat=other
+}
+
+long=subset(BullData2021,)
+subset(BullData2021,BulNum==2)$Longitude
+
+
+year=2021
+get(paste0('BullData',year))
+
+
+
+subset(BullData2021,BulNum==2)
